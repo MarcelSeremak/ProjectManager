9e82526a350d312a4eb435aac7e775b043536f85
Marcel Seremak <marcelseremak47@gmail.com>
2024-03-15 19:27:11 +0100

    Adding route 'about me' and everything about me on this page

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, render_template, redirect, url_for
+from flask_wtf import FlaskForm, Form
+from flask_bootstrap import Bootstrap
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import LoginForm, RegisterForm
+from flask_bootstrap import render_form
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "ciouasbocaboxoanxa"
+Bootstrap(app)
 
 
 @app.route("/")
@@ -18,13 +27,39 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
 
-@app.route("/register")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for("starting_page"))
+
+    return render_template("login.html", form=form)
+
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect(url_for("starting_page"))
+
+    return render_template("register.html", form=form)
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for("starting_page"))
+
+
+@app.route("/random_project")
+def random_project():
+    return render_template("random-project.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
 
 
 if __name__ == "__main__":
