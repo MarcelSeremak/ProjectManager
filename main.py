0828c5da5501016561c2ae3c7e0c800576352764
Marcel Seremak <marcelseremak47@gmail.com>
2024-03-15 18:16:21 +0100

    Finishing homepage look and starting to work on login and register forms and routes

--- main.py
+++ main.py
@@ -18,5 +18,14 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
